81564d75fb7b46a936d624056b01a22b52bf5521
justinPhung803 <phunglamtung2003@gmail.com>
2025-05-07 21:48:44 -0400

    fix harshing bug

--- api/models/User.js
+++ api/models/User.js
@@ -59,15 +59,6 @@ const userSchema = new mongoose.Schema({
 );
 
 userSchema.pre("save", async function (next) {
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-};
-
-userSchema.pre("save", async function (next) {
 if (!this.isModified("password")) {
     return next();
 }
@@ -75,6 +66,11 @@ if (!this.isModified("password")) {
 this.password = await bcrypt.hash(this.password, 10);
 next();
 });
+
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+};
 
 const User = mongoose.model("User", userSchema);
 
